7ea31cc8cfcebe6ff9355e237d1adfbb716b57c8
root <raghugupta@dbs.com>
2018-05-30 17:10:01 +0800

    division added.

---
+++ src/main/java/com/edureka/git/CalculatorImpl.java
@@ -0,0 +1,24 @@
+package com.edureka.git;
+
+import java.math.BigInteger;
+
+public class CalculatorImpl implements ICalculator {
+
+	public Long add(Integer a, Integer b) {
+		return null;
+	}
+
+	public Long sub(Integer a, Integer b) {
+		return null;
+	}
+
+	public Long mul(Integer a, Integer b) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Long div(Integer a, Integer b) {
+		return new BigInteger(a+"").divide(new BigInteger(b+"")).longValue();
+	}
+
+}
