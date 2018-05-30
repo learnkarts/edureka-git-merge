36d584cbfd637805e93c0a3ca9737a8a0401320f
root <raghugupta@dbs.com>
2018-05-30 16:49:25 +0800

    interface added

---
+++ src/main/java/com/edureka/git/ICalculator.java
@@ -0,0 +1,13 @@
+package com.edureka.git;
+
+public interface ICalculator {
+
+	Long add(Integer a, Integer b );
+	
+	Long sub(Integer a, Integer b );
+	
+	Long mul(Integer a, Integer b );
+	
+	Long div(Integer a, Integer b );
+	
+}
